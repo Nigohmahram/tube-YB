fc8e17dfa82fa0e76b99e68a8899cfbb68843a81
Usmonov Hojiakbar <usmonovmaxsudovich@gmail.com>
2023-10-04 17:17:49 +0900

    React router Dom v6

---
+++ src/components/chanel/chanel.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
+const Chanel = () => {
+	return (
+		<Link to={'/'}>
+			<Button>Main</Button>
+		</Link>
+	);
+};
+
+export default Chanel;
---
+++ src/components/main/main.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
+
+const Main = () => {
+	return (
+		<Link to={'/chanel'}>
+			<Button>button</Button>
+		</Link>
+	);
+};
+
+export default Main;
